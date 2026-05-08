a1a295fb8bc7f19b80e364141e139a875c1854ba
Ado-do <alonbustos2021@udec.cl>
2026-05-08 05:26:50 -0400

    agregar descarga de textos al script de inicialización

--- initialize_project.sh
+++ initialize_project.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-chmod +x compile.sh
-chmod +x execute_benchmarks.sh
-chmod +x execute_tests.sh
-chmod +x plot.sh
+declare -A texts=(
+    ["english.50MB"]="https://pizzachili.dcc.uchile.cl/texts/nlang/english.50MB.gz"
+    ["dna.50MB"]="https://pizzachili.dcc.uchile.cl/texts/dna/dna.50MB.gz"
+    ["xml.50MB"]="https://pizzachili.dcc.uchile.cl/texts/xml/dblp.xml.50MB.gz"
+)
 
+echo "* Descargando textos desde Pizza&Chili..."
+for name in "${!texts[@]}"; do
+    url="${texts[$name]}"
+    file="input/${name}"
+    file_gz="${file}.gz"
+
+    if [ -f "$file" ]; then
+        echo "Texto $file ya existe!"
+        continue
+    fi
+
+    wget -q "$url" -O "$file_gz"
+    gunzip -c "$file_gz" > "$file"
+    rm "$file_gz"
+
+    echo "Texto $file descargado!"
+done
+
+chmod +x compile.sh execute_benchmarks.sh execute_tests.sh plot.sh
 cmake -S . -B build
 ./compile.sh
